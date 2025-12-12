8621e84de67759563732ef9aab35e359333a144b
Vili <vilibald.music@gmail.com>
2025-12-12 18:07:04 +0100

    vjezbe

---
+++ Vj1.py
@@ -0,0 +1,17 @@
+
+
+broj = 7 / 3
+
+print(7 // 3)
+print(7/3) # odma pretvara u float 
+
+
+# pythona nije briga za promjenu varijabli niti tipova 
+
+broj = "neki string"
+print(broj)
+
+
+# provjera tipa 
+
+print(1 ,2,3 , broj, " je ", type(broj))
